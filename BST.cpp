45eb5925edb349dd20d8a4432ec8e175b8156f5d
Hinna Parwez <78386492+HinnaP@users.noreply.github.com>
2024-10-28 16:18:25 -0400

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -1,1 +1,20 @@
+# include <iostream>
+# include <cstdlib>
+#include "BST.h"
+using namespace std;
 
+// defining from binary search tree class
+
+BST::BST()
+{
+    root = NULL;
+}
+BST::node* BST::CreateLeaf(int key)
+{
+    node* n = new node;
+    n->key = key;
+    n->left = NULL;
+    n->right = NULL;
+
+    return n;
+}
