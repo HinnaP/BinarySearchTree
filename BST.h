b9c6fc18259dfaf2d115e79b14d3f8e982ea295e
Hinna Parwez <78386492+HinnaP@users.noreply.github.com>
2024-10-28 16:14:40 -0400

    Update BST.h

--- BST.h
+++ BST.h
@@ -1,1 +1,14 @@
-
+class BST 
+{
+private:
+    struct node
+    {
+        int key;
+        node*left;
+        node*right;
+    };
+    node* root;
+public:
+    BST();
+    node* CreateLeaf(int key);
+};
